15343483104b9c544e46ae926c13e0f46a8322c7
Lesya <povolotslaya.ll@mail.ru>
2023-02-25 21:32:46 +0300

    Написали программу

---
+++ Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array1 = { "Hello", "2", "“world”", "Mia", ":-)" };
+
+PrintArray(array1);
+string [] arraynew = NewArray(array1);
+PrintArray1(arraynew);
+
+
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("] => ");
+}
+
+
+void PrintArray1(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+string [] NewArray(string[] arr)
+{
+    int length = arr.Length;
+    int count = 0;
+    string[] array1 = new string[length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            array1[count] = arr[i];
+            count ++;
+        }
+
+    }
+    Array.Resize(ref array1, count);
+    return array1;
+}
